2a375edd00575ce9e79421107147583e54e5430b
GJHeyes <gregory-heyes@hotmail.com>
2023-05-04 13:52:23 +0100

    first commit

---
+++ src/main/java/com/heyesinc/api/mindstamp/MindstampApplication.java
@@ -0,0 +1,13 @@
+package com.heyesinc.api.mindstamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindstampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindstampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heyesinc/api/mindstamp/MindstampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyesinc.api.mindstamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindstampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
